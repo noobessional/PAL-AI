1193584c6e8cc3f3a74395e19e4fdc9911ea73ca
stephjohndelossantos <stephjohndelossantos@gmail.com>
2024-10-04 22:21:38 +0800

    First commit
    First upload: 10/04/2024

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AuthenticationLayout = () => {
+  return (
+    <View>
+      <Text>AuthenticationLayout</Text>
+    </View>
+  );
+};
+
+export default AuthenticationLayout;
+
+const styles = StyleSheet.create({});
---
+++ app/index.jsx
@@ -0,0 +1,43 @@
+import { ScrollView, Text, View, Image, ImageBackground } from "react-native";
+import { Link, Redirect, router, Router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
+
+export default function App() {
+  return (
+    <SafeAreaView className="bg-[#ffffff] h-full">
+      <ImageBackground
+        source={images.background1}
+        className="flex-1 w-full"
+        imageStyle={{ opacity: 0.2 }}
+      >
+        <ScrollView contentContainerStyle={{ height: "100%" }}>
+          <View className="w-full justify-center items-center h-[85vh] px-7">
+            <Image
+              source={images.logo}
+              className="w-[200px] h-[200px]"
+              resizeMode="contain"
+            />
+            <Text className="text-[35px] font-pmedium text-[#4B4B4B] mt-5 text-center">
+              WELCOME TO <Text className="font-psemibold">PAL-AI</Text>
+            </Text>
+
+            <CustomButton
+              title="GET STARTED"
+              handlePress={() => router.push("/sign-up")}
+              containerStyles="w-full mt-3"
+            />
+            <Text className="mt-3 font-pregular text-base text-[#4B4B4B]">
+              Have an account?{" "}
+              <Text className="font-pbold text-secondary">Login</Text>
+            </Text>
+          </View>
+        </ScrollView>
+      </ImageBackground>
+      <StatusBar backgroundColor="#228B22" style="light" />
+    </SafeAreaView>
+  );
+}
